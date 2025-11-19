57d87a515b71572d85e52685fecf4d0e9f036241
vicus-ux <viktoriadubskih@gmail.com>
2025-11-19 11:49:42 +0300

    Remove build directory from version control

--- build/_deps/googletest1-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/gitprojs/Data-Structures/build/_deps/googletest1-src
-# Build directory: C:/gitprojs/Data-Structures/build/_deps/googletest1-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/_deps/googletest1-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/gitprojs/Data-Structures/build/_deps/googletest1-src/googletest
-# Build directory: C:/gitprojs/Data-Structures/build/_deps/googletest1-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
